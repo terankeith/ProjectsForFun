f647c6230b789c5539300c6a6dc014f8eb69a6d1
terankeith <keitht@csp.edu>
2015-10-29 23:05:40 -0500

    Updating BubbleGum

---
+++ BubbleGum/BubbleGum/Scripts/App/BubbleGum.js
@@ -0,0 +1,242 @@
+﻿$(function () {
+
+    $('#bass-1').hide().delay(500).fadeIn();
+    $('#bass-2').hide().delay(550).fadeIn();
+    $('#bass-3').hide().delay(600).fadeIn();
+    $('#bass-4').hide().delay(650).fadeIn();
+    $('#bass-5').hide().delay(700).fadeIn();
+    $('#bass-6').hide().delay(750).fadeIn();
+    $('#bass-7').hide().delay(800).fadeIn();
+    $('#bass-8').hide().delay(850).fadeIn();
+    $('#bass-9').hide().delay(900).fadeIn();
+    $('#bass-10').hide().delay(950).fadeIn();
+    $('#bass-11').hide().delay(1000).fadeIn();
+    $('#bass-12').hide().delay(1050).fadeIn();
+    $('#bass-13').hide().delay(1100).fadeIn();
+    $('#bass-14').hide().delay(1150).fadeIn();
+    $('#tenor-1').hide().delay(1200).fadeIn();
+    $('#tenor-2').hide().delay(1250).fadeIn();
+    $('#tenor-3').hide().delay(1300).fadeIn();
+    $('#tenor-4').hide().delay(1350).fadeIn();
+    $('#tenor-5').hide().delay(1400).fadeIn();
+    $('#tenor-6').hide().delay(1450).fadeIn();
+    $('#tenor-7').hide().delay(1500).fadeIn();
+    $('#tenor-8').hide().delay(1550).fadeIn();
+    $('#bass-15').hide().delay(1600).fadeIn();
+    $('#bass-16').hide().delay(1650).fadeIn();
+    $('#bass-17').hide().delay(1700).fadeIn();
+    $('#bass-18').hide().delay(1750).fadeIn();
+    $('#tenor-9').hide().delay(1800).fadeIn();
+    $('#tenor-10').hide().delay(1850).fadeIn();
+    $('#alto-1').hide().delay(1900).fadeIn();
+    $('#alto-2').hide().delay(1950).fadeIn();
+    $('#alto-3').hide().delay(2000).fadeIn();
+    $('#alto-4').hide().delay(2050).fadeIn();
+    $('#tenor-11').hide().delay(2100).fadeIn();
+    $('#tenor-12').hide().delay(2150).fadeIn();
+    $('#bass-19').hide().delay(2200).fadeIn();
+    $('#bass-20').hide().delay(2250).fadeIn();
+    $('#bass-21').hide().delay(2300).fadeIn();
+    $('#bass-22').hide().delay(2350).fadeIn();
+    $('#tenor-13').hide().delay(2400).fadeIn();
+    $('#tenor-14').hide().delay(2450).fadeIn();
+    $('#alto-5').hide().delay(2500).fadeIn();
+    $('#soprano-1').hide().delay(2550).fadeIn();
+    $('#soprano-2').hide().delay(2600).fadeIn();
+    $('#alto-6').hide().delay(2650).fadeIn();
+    $('#tenor-15').hide().delay(2700).fadeIn();
+    $('#tenor-16').hide().delay(2750).fadeIn();
+    $('#bass-23').hide().delay(2800).fadeIn();
+    $('#bass-24').hide().delay(2850).fadeIn();
+    $('#bass-25').hide().delay(2900).fadeIn();
+    $('#bass-26').hide().delay(2950).fadeIn();
+    $('#tenor-17').hide().delay(3000).fadeIn();
+    $('#tenor-18').hide().delay(3050).fadeIn();
+    $('#alto-7').hide().delay(3100).fadeIn();
+    $('#soprano-3').hide().delay(3150).fadeIn();
+    $('#soprano-4').hide().delay(3200).fadeIn();
+    $('#alto-8').hide().delay(3250).fadeIn();
+    $('#tenor-19').hide().delay(3300).fadeIn();
+    $('#tenor-20').hide().delay(3350).fadeIn();
+    $('#bass-27').hide().delay(3400).fadeIn();
+    $('#bass-28').hide().delay(3450).fadeIn();
+    $('#bass-29').hide().delay(3500).fadeIn();
+    $('#bass-30').hide().delay(3550).fadeIn();
+    $('#tenor-21').hide().delay(3600).fadeIn();
+    $('#tenor-22').hide().delay(3650).fadeIn();
+    $('#alto-9').hide().delay(3700).fadeIn();
+    $('#soprano-5').hide().delay(3750).fadeIn();
+    $('#soprano-6').hide().delay(3800).fadeIn();
+    $('#alto-10').hide().delay(3850).fadeIn();
+    $('#tenor-23').hide().delay(3900).fadeIn();
+    $('#tenor-24').hide().delay(3950).fadeIn();
+    $('#bass-31').hide().delay(4000).fadeIn();
+    $('#bass-32').hide().delay(4050).fadeIn();
+    $('#bass-33').hide().delay(4100).fadeIn();
+    $('#bass-34').hide().delay(4150).fadeIn();
+    $('#tenor-25').hide().delay(4200).fadeIn();
+    $('#tenor-26').hide().delay(4250).fadeIn();
+    $('#alto-11').hide().delay(4300).fadeIn();
+    $('#soprano-7').hide().delay(4350).fadeIn();
+    $('#soprano-8').hide().delay(4400).fadeIn();
+    $('#alto-12').hide().delay(4450).fadeIn();
+    $('#tenor-27').hide().delay(4500).fadeIn();
+    $('#tenor-28').hide().delay(4550).fadeIn();
+    $('#bass-35').hide().delay(4600).fadeIn();
+    $('#bass-36').hide().delay(4650).fadeIn();
+    $('#bass-37').hide().delay(4700).fadeIn();
+    $('#bass-38').hide().delay(4750).fadeIn();
+    $('#tenor-29').hide().delay(4800).fadeIn();
+    $('#tenor-30').hide().delay(4850).fadeIn();
+    $('#alto-13').hide().delay(4900).fadeIn();
+    $('#soprano-9').hide().delay(4950).fadeIn();
+    $('#soprano-10').hide().delay(5000).fadeIn();
+    $('#alto-14').hide().delay(5050).fadeIn();
+    $('#tenor-31').hide().delay(5100).fadeIn();
+    $('#tenor-32').hide().delay(5150).fadeIn();
+    $('#bass-39').hide().delay(5200).fadeIn();
+    $('#bass-40').hide().delay(5250).fadeIn();
+    $('#bass-41').hide().delay(5300).fadeIn();
+    $('#bass-42').hide().delay(5350).fadeIn();
+    $('#tenor-33').hide().delay(5400).fadeIn();
+    $('#tenor-34').hide().delay(5450).fadeIn();
+    $('#alto-15').hide().delay(5500).fadeIn();
+    $('#soprano-11').hide().delay(5550).fadeIn();
+    $('#soprano-12').hide().delay(5600).fadeIn();
+    $('#alto-16').hide().delay(5650).fadeIn();
+    $('#tenor-35').hide().delay(5700).fadeIn();
+    $('#tenor-36').hide().delay(5750).fadeIn();
+    $('#bass-43').hide().delay(5800).fadeIn();
+    $('#bass-44').hide().delay(5850).fadeIn();
+    $('#bass-45').hide().delay(5900).fadeIn();
+    $('#bass-46').hide().delay(5950).fadeIn();
+    $('#tenor-37').hide().delay(6000).fadeIn();
+    $('#tenor-38').hide().delay(6150).fadeIn();
+    $('#alto-17').hide().delay(6200).fadeIn();
+    $('#alto-18').hide().delay(6250).fadeIn();
+    $('#alto-19').hide().delay(6300).fadeIn();
+    $('#alto-20').hide().delay(6350).fadeIn();
+    $('#tenor-39').hide().delay(6400).fadeIn();
+    $('#tenor-40').hide().delay(6450).fadeIn();
+    $('#bass-47').hide().delay(6500).fadeIn();
+    $('#bass-48').hide().delay(6550).fadeIn();
+    $('#bass-49').hide().delay(6600).fadeIn();
+    $('#bass-50').hide().delay(6650).fadeIn();
+    $('#tenor-41').hide().delay(6700).fadeIn();
+    $('#tenor-42').hide().delay(6750).fadeIn();
+    $('#tenor-43').hide().delay(6800).fadeIn();
+    $('#tenor-44').hide().delay(6850).fadeIn();
+    $('#tenor-45').hide().delay(6900).fadeIn();
+    $('#tenor-46').hide().delay(6950).fadeIn();
+    $('#tenor-47').hide().delay(7000).fadeIn();
+    $('#tenor-48').hide().delay(7050).fadeIn();
+    $('#bass-51').hide().delay(7100).fadeIn();
+    $('#bass-52').hide().delay(7150).fadeIn();
+    $('#bass-53').hide().delay(7200).fadeIn();
+    $('#bass-54').hide().delay(7250).fadeIn();
+    $('#bass-55').hide().delay(7300).fadeIn();
+    $('#bass-56').hide().delay(7350).fadeIn();
+    $('#bass-57').hide().delay(7400).fadeIn();
+    $('#bass-58').hide().delay(7450).fadeIn();
+    $('#bass-59').hide().delay(7500).fadeIn();
+    $('#bass-60').hide().delay(7550).fadeIn();
+    $('#bass-61').hide().delay(7600).fadeIn();
+    $('#bass-62').hide().delay(7650).fadeIn();
+    $('#bass-63').hide().delay(7700).fadeIn();
+    $('#bass-64').hide().delay(7750).fadeIn();
+    
+
+
+
+    //$(".bass").animate({
+    //    margin: 500
+    //}, {
+    //    queue: false,
+    //    duration: 10000,
+    //    step: function (now, fx) {
+    //        $(".bass:nth(4)").css("bottom", now);
+    //    }
+    //});
+    //$(".bass:first").animate({
+    //    top: 200
+    //}, {
+    //    queue: false,
+    //    duration: 10000,
+    //    step: function (now, fx) {
+    //        $(".bass:nth(4), .bass:nth(5)").css("bottom", now);
+    //    }
+    //});
+
+
+    //function move(step1, step2, step3) {
+    //    $('#bass-64').animate({
+    //        left: '+=' + step1,
+    //    }, 1000, function () {
+    //        $(this).animate({
+    //            top: '+=' + step2,
+    //        }, 1000, function () {
+    //            $(this).animate({
+    //                left: '-=' + step3
+    //            }, 1000);
+    //        });
+    //    });
+    //}
+
+    //$(function () {
+    //    move(100, 50, 70);
+    //});
+
+
+
+
+    //$('#bass-1').css("transform", "translateX(500px)");
+
+    //$('.bass, .tenor, .alto, .soprano').addClass('animated bounceInDown').one(animationEnd, function () {
+    //    $(this).removeClass('animated bounceInDown').addClass('animated shake');
+    //});
+
+    //var top = 0;
+    //var left = 0;
+
+    //$('#bass-1').css('position', 'fixed');
+    //$('#bass-1').css('top', top);
+    //$('#bass-1').css('left', left);
+    //    var i = 0;
+
+    //    var bass1Movements = [
+    //        {
+    //            x: 0,
+    //            y: 0,
+    //            cssClass: 'animated bounceInDown',
+    //            cssClassToRemove: '',
+    //            delay: 0,
+    //            alreadyPlayed: false
+    //        }
+    //    ];
+
+    //    var timeElapsed = 0;
+
+    //    setInterval(function () {
+    //        var moveBass1 = bass1Movements[i];
+
+    //        var moveToMake = bass1Movements.filter(function (item) {
+    //            return item.delay <= timeElapsed && alreadyPlayed == false;
+    //        });
+
+    //        for (var j = 0; j < moveToMake; j++) {
+    //            $('#bass-1').addClass(moveToMake[j].cssClass);
+    //            moveToMake[j].alreadyPlayed = true;
+    //        }
+
+    //        timeElapsed += 10;
+    //        i++;
+    //        //$('#bass-1').css('top', top + 'px');
+    //        //$('#bass-1').css('left', left + 'px');
+    //        //$('#bass-2').css('top', top + 10 + 'px');
+    //        //$('#bass-2').css('left', left + 10 + 'px');
+
+    //    }, 10)
+
+    //    //$('#bass-64').addClass('animated bounceInDown').animate();
+});
+
